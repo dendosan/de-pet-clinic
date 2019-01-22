6ab1d584725b92b85b9a4f25d5b4010dcf48413c
elliotd <elliotd@cisco.com>
2019-01-22 00:01:12 -0500

    refactor to common interface.  closes #11.

--- pet-clinic-data/src/main/java/com/example/petclinicdata/services/PetService.java
+++ pet-clinic-data/src/main/java/com/example/petclinicdata/services/PetService.java
@@ -1,12 +1,7 @@
 package com.example.petclinicdata.services;
 
-import java.util.Set;
-
 import com.example.petclinicdata.model.Pet;
 
-public interface PetService {
+public interface PetService extends CrudService<Pet, Long> {
 
-   Pet findById(Long id);
-   Pet save(Pet pet);
-   Set<Pet> findAll();
 }
