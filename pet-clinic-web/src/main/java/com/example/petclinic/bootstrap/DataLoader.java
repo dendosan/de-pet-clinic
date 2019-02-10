97a4405bebf04b2fe2f48c0e7a38782394fb88b4
elliotd <elliotd@cisco.com>
2019-02-09 20:22:50 -0500

    implement list of owners on index page. closes #16.

--- pet-clinic-web/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
@@ -7,8 +7,6 @@ import com.example.petclinic.model.Owner;
 import com.example.petclinic.model.Vet;
 import com.example.petclinic.services.OwnerService;
 import com.example.petclinic.services.VetService;
-import com.example.petclinic.services.map.OwnerServiceMap;
-import com.example.petclinic.services.map.VetServiceMap;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -17,8 +15,8 @@ public class DataLoader implements CommandLineRunner {
    private final VetService vetService;
    
    public DataLoader(OwnerService ownerService, VetService vetService) {
-      this.ownerService = new OwnerServiceMap();
-      this.vetService = new VetServiceMap();
+      this.ownerService = ownerService;
+      this.vetService = vetService;
    }
 
    @Override
@@ -29,14 +27,14 @@ public class DataLoader implements CommandLineRunner {
       owner1.setFirstName("Michael");
       owner1.setLastName("Weston");
       
-      ownerService.save(owner1);
+      this.ownerService.save(owner1);
       
       Owner owner2 = new Owner();
       owner2.setId(2L);
       owner2.setFirstName("Fiona");
       owner2.setLastName("Glenanne");
       
-      ownerService.save(owner2);
+      this.ownerService.save(owner2);
       
       System.out.println("Loaded Owners...");
       
@@ -45,14 +43,14 @@ public class DataLoader implements CommandLineRunner {
       vet1.setFirstName("Sam");
       vet1.setLastName("Axe");
       
-      vetService.save(vet1);
+      this.vetService.save(vet1);
       
       Vet vet2 = new Vet();
       vet2.setId(2L);
       vet2.setFirstName("Jessie");
       vet2.setLastName("Porter");
       
-      vetService.save(vet2);
+      this.vetService.save(vet2);
       
       System.out.println("Loaded Vets...");
    }
