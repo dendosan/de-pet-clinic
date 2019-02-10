bdff11e012269b40340767fce1e75b6ef91ab1f1
elliotd <elliotd@cisco.com>
2019-02-09 20:28:52 -0500

    add vets to index page.  closes #15

--- pet-clinic-web/src/main/java/com/example/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/controllers/VetController.java
@@ -1,14 +1,26 @@
 package com.example.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.petclinic.services.VetService;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+   private final VetService vetService;
+   
+   public VetController(VetService vetService) {
+      this.vetService = vetService;
+   }
+   
    @RequestMapping({"", "/", "/index", "/index.html"})
-   public String listVets() {
+   public String listVets(Model model) {
+      
+      model.addAttribute("vets", vetService.findAll());
+      
       return "vets/index";
    }
 }
