7f4b0d04a6925b6ee533b30897e7de8163663ee0
elliotd <elliotd@cisco.com>
2019-02-12 14:40:33 -0500

    created visits and add pet to owner.  closes #26

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package com.example.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
    private static final long serialVersionUID = -4036215716934327985L;
+   
+   private Set<Pet> pets;
+
+   public Set<Pet> getPets() {
+      return pets;
+   }
+
+   public void setPets(Set<Pet> pets) {
+      this.pets = pets;
+   }
 
 }
