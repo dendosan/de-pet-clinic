f5e03761496cc19c84c99ca336cd2acde332fb6a
elliotd <elliotd@cisco.com>
2019-02-13 17:10:49 -0500

    fixed menu links.  closes #45

--- pet-clinic-web/src/main/java/com/example/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/controllers/VetController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.petclinic.services.VetService;
 
-@RequestMapping("/vets")
+@RequestMapping({"/vets", "vets.html"})
 @Controller
 public class VetController {
 
