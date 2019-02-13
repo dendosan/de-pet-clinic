2ea7b7e825bed377a8471b3b742e02e62a3ae54a
elliotd <elliotd@cisco.com>
2019-02-13 17:00:06 -0500

    add speciality to vet service map

--- pet-clinic-data/src/main/java/com/example/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/map/VetServiceMap.java
@@ -4,12 +4,20 @@ import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
+import com.example.petclinic.model.Speciality;
 import com.example.petclinic.model.Vet;
+import com.example.petclinic.services.SpecialityService;
 import com.example.petclinic.services.VetService;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
+   private SpecialityService specialityService;
+   
+   public VetServiceMap(SpecialityService specialityService) {
+      this.specialityService = specialityService;
+   }
+   
    @Override
    public Set<Vet> findAll() {
       return super.findAll();
@@ -22,6 +30,15 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
    @Override
    public Vet save(Vet object) {
+      
+      if (object.getSpecialities().size() > 0) {
+         object.getSpecialities().forEach(speciality -> {
+            if (speciality.getId() == null) {
+               Speciality savedSpeciality = specialityService.save(speciality);
+               speciality.setId(savedSpeciality.getId());
+            }
+         });
+      }
       return super.save(object);
    }
 
