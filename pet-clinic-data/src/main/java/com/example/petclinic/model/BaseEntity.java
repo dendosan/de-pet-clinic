04cbc90b412d95cbf6363957c4e999c1e3768863
elliotd <elliotd@cisco.com>
2019-02-14 14:39:30 -0500

    JPA annotations for base entity. closes #35

--- pet-clinic-data/src/main/java/com/example/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/BaseEntity.java
@@ -2,10 +2,18 @@ package com.example.petclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
    private static final long serialVersionUID = -4626827386785433604L;
 
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
 
    public Long getId() {
