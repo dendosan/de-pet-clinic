c00c7b137a5c9d2f99dd71efac2252d7f9be5fc6
elliotd <elliotd@cisco.com>
2019-02-14 14:58:10 -0500

    jpa annotations to speciality and vet.  closes #37

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Speciality.java
@@ -1,12 +1,16 @@
 package com.example.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
-   /**
-    * 
-    */
    private static final long serialVersionUID = 4182462080495955710L;
 
+   @Column(name = "description")
    private String description;
 
    public String getDescription() {
--- pet-clinic-data/src/main/java/com/example/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Vet.java
@@ -3,9 +3,22 @@ package com.example.petclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
    private static final long serialVersionUID = -2662774399603335422L;
 
+   @ManyToMany(fetch = FetchType.EAGER)
+   @JoinTable(name = "vet_specialities",
+   joinColumns = @JoinColumn(name = "vet_id"),
+   inverseJoinColumns = @JoinColumn(name = "speciality_id"))
    private Set<Speciality> specialities = new HashSet<>();
 
    public Set<Speciality> getSpecialities() {
