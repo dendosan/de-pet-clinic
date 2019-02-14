c0c27586adcc92a9b0b159cef40f54dc8f842c72
elliotd <elliotd@cisco.com>
2019-02-14 15:06:22 -0500

    jpa mappings to pet and visit.  closes #38

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Visit.java
@@ -2,15 +2,25 @@ package com.example.petclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
-   /**
-    * 
-    */
    private static final long serialVersionUID = 5963760232333236055L;
 
+   @Column(name = "date")
    private LocalDate date;
+   @Column(name = "description")
    private String description;
+
+   @ManyToOne
+   @JoinColumn(name = "pet_id")
    private Pet pet;
 
    public LocalDate getDate() {
